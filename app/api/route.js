d04de68c117ae2d17bea96e3ccd55bfa2fb7a34b
mohotasir <mohotasir1036@gmail.com>
2025-02-17 20:58:30 +0600

    feat: add API route for fetching todos and MongoDB connection setup

---
+++ app/api/route.js
@@ -0,0 +1,19 @@
+import { connectDB } from "@/lib/connectDB";
+
+export const GET = async () => {
+  try {
+    const db = await connectDB();
+    const todos = db.collection("todos");
+    const allTodos = await todos.find({}).toArray();
+
+    return new Response(JSON.stringify(allTodos), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+};
